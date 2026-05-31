c8ab89329ad2d24963f2a8630c64af2f3d640e5e
Alejandro A. <xelark92@gmail.com>
2026-05-31 17:55:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pokebestbcn-v48';
+const CACHE_NAME = 'pokebestbcn-v49';
 const ASSETS = ['/pokebestbcn/index.html','/pokebestbcn/manifest.json','/pokebestbcn/shadow_icon.png','/pokebestbcn/icon-192.png','/pokebestbcn/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
