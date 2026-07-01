0e18add9b367099040f6845805cc87b631416a11
Alejandro A. <xelark92@gmail.com>
2026-07-01 19:45:19 +0200

    Update CACHE_NAME for service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pokebestbcn-v92';
+const CACHE_NAME = 'pokebestbcn-v92.2';
 const ASSETS = ['/pokebestbcn/index.html','/pokebestbcn/manifest.json','/pokebestbcn/shadow_icon.png','/pokebestbcn/icon-192.png','/pokebestbcn/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
